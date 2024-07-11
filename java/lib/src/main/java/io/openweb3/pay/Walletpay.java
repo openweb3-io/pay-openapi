6319eb0a37de4933aa3a153c6d509304b754582f
Pharaoh <pharaoh@mtsocialdao.com>
2024-07-11 16:25:28 +0800

    chore: update version number

--- java/lib/src/main/java/io/openweb3/pay/Walletpay.java
+++ java/lib/src/main/java/io/openweb3/pay/Walletpay.java
@@ -20,7 +20,7 @@ import io.openweb3.pay.exceptions.SigningException;
 import java.io.IOException;
 
 public final class Walletpay {
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.2.0";
 	private final Order order;
 	private final Account account;
 	private final Endpoint endpoint;
